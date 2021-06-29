f9be6a1e70480bca77daffaa4b1cbd45ae447a24
Eric Vasconcelos de Oliveira <eric.oliveira@printi.com.br>
2021-06-28 21:19:20 -0300

    Create about app screen

---
+++ screens/AboutApp/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export default function AboutApp() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>AboutApp</Text>
+    </View>
+  );
+}
