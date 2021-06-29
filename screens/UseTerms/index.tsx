bb6b65951db7b7b0b9562e8f87632a6d8491e6da
Eric Vasconcelos de Oliveira <eric.oliveira@printi.com.br>
2021-06-28 21:18:41 -0300

    Create user terms screen

---
+++ screens/UseTerms/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export default function UseTerms() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>UseTerms</Text>
+    </View>
+  );
+}
