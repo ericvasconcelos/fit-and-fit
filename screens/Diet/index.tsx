f9461e400c27fe126baffaa91cdc342ab0ba6491
Eric Vasconcelos de Oliveira <eric.oliveira@printi.com.br>
2021-06-28 21:19:34 -0300

    Create diet page screen

---
+++ screens/Diet/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export default function Diet() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Diet</Text>
+    </View>
+  );
+}
